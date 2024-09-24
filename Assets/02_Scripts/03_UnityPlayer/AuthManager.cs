9e1a4d3a16d47f5975505633fde9ccdb476e1099
LeeJaeHyun <myleje@gmail.com>
2024-09-24 15:26:49 +0900

    .

--- Assets/02_Scripts/03_UnityPlayer/AuthManager.cs
+++ Assets/02_Scripts/03_UnityPlayer/AuthManager.cs
@@ -1,4 +1,5 @@
 using TMPro;
+using Unity.Services.Authentication;
 using Unity.Services.Authentication.PlayerAccounts;
 using Unity.Services.Core;
 using UnityEngine;
@@ -15,10 +16,21 @@ namespace AuthUnityPlayer
         {
             await UnityServices.InitializeAsync();
 
+            // 로그인 후 콜백 연결
+            PlayerAccountService.Instance.SignedIn += OnSignedIn;
+
             signInButton.onClick.AddListener(async () =>
             {
                 await PlayerAccountService.Instance.StartSignInAsync();
             });
+        }
+
+        // 로그인 프로세스 
+        private async void OnSignedIn()
+        {
+            string accessToken = PlayerAccountService.Instance.AccessToken;
+            await AuthenticationService.Instance.SignInWithUnityAsync(accessToken);
+            Debug.Log("로그인 완료");
         }
     }
 }
