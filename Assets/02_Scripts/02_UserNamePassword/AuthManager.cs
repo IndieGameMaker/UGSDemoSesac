e056fbf9b786e997f8c9de618e53996a38a8f6d2
LeeJaeHyun <myleje@gmail.com>
2024-09-24 14:28:22 +0900

    .

--- Assets/02_Scripts/02_UserNamePassword/AuthManager.cs
+++ Assets/02_Scripts/02_UserNamePassword/AuthManager.cs
@@ -1,5 +1,8 @@
 
+using System.Threading.Tasks;
 using TMPro;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,5 +12,32 @@ namespace AuthUserNamePassword
     {
         [SerializeField] private TMP_InputField userNameIf, passwordIf;
         [SerializeField] private Button signUpButton, signInButton;
+
+        private async void Awake()
+        {
+            await UnityServices.InitializeAsync();
+            Debug.Log("UGS 초기화");
+
+
+        }
+
+        // 회원가입 로직
+        private async Task SignUpAsync(string userName, string password)
+        {
+            try
+            {
+                await AuthenticationService.Instance.SignUpWithUsernamePasswordAsync(userName, password);
+                Debug.Log("회원 가입 완료");
+            }
+            catch (AuthenticationException e)
+            {
+                Debug.LogException(e);
+            }
+            catch (RequestFailedException e)
+            {
+                Debug.LogException(e);
+            }
+        }
+        // 로그인 로직
     }
 }
