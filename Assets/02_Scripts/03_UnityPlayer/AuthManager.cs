28c0423b7fa4eda13199c87ee04a4051b892493c
LeeJaeHyun <myleje@gmail.com>
2024-09-24 15:16:51 +0900

    .

--- Assets/02_Scripts/03_UnityPlayer/AuthManager.cs
+++ Assets/02_Scripts/03_UnityPlayer/AuthManager.cs
@@ -1,4 +1,6 @@
 using TMPro;
+using Unity.Services.Authentication.PlayerAccounts;
+using Unity.Services.Core;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,7 +8,17 @@ namespace AuthUnityPlayer
 {
     public class AuthManager : MonoBehaviour
     {
-        [SerializeField] private Button siginInButton, signOutButton, playerNameSaveButton;
+        [SerializeField] private Button signInButton, signOutButton, playerNameSaveButton;
         [SerializeField] private TMP_InputField playerNameIf;
+
+        private async void Awake()
+        {
+            await UnityServices.InitializeAsync();
+
+            signInButton.onClick.AddListener(async () =>
+            {
+                await PlayerAccountService.Instance.StartSignInAsync();
+            });
+        }
     }
 }
